e25d8bfe4b04da056138107be704fab3c3c8bd16
Subhash-1994 <subhash.v.nair8162@gmail.com>
2025-10-05 14:45:38 +0530

    Initial commit: Create Java project with main class

---
+++ src/mainapp/MainApp.java
@@ -0,0 +1,12 @@
+package mainapp;
+
+public class MainApp {
+    public static void main(String[] args) {
+       
+        System.out.println("Hello, GitHub! This is my first initial commit from Eclipse.");
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
